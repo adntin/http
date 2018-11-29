d1567d494288090588591ff55432895ebec479b6
Devin Lin <devin.lin@ringcentral.com>
2018-11-29 19:58:48 +0800

    Cookie set domain

--- cookie/README.md
+++ cookie/README.md
@@ -9,3 +9,18 @@ key=value: 键值对
 max-age: 设置 cookie 过期时间, 单位秒, 可以在控制台(Application, Console)验证, document.cookie
 HttpOnly: 浏览器不可读取, 只有服务器可以读取, 可以在控制台(Application, Console)验证, document.cookie
 domain: 只能设置当前域的 cookie, 二级域名想用一级域名的 cookie, 需要先访问一级域名
+
+# 注意
+
+如果没有设置过期时间, 浏览器关闭时 cookie 就失效
+
+# 设置 domain 步骤
+
+1. 安装 Chrome extensions: Host Switch Plus
+2. 设置 Host Switch Plus
+   IP: 127.0.0.1:8888 Domain: test.com
+   IP: 127.0.0.1:8888 Domain: a.test.com
+   IP: 127.0.0.1:8888 Domain: b.test.com
+3. 查看 URL, console.log(request.url);
+4. 访问 test.com
+5. 访问 a.test.com
--- cookie/server.js
+++ cookie/server.js
@@ -5,15 +5,15 @@ http
   .createServer(function(request, response) {
     const { url } = request;
     const html = fs.readFileSync("index.html", "utf8");
+    // 1. 设置其它
     response.writeHead(200, {
       "Content-Type": "text/html",
       // "Set-Cookie": "a=1",
       "Set-Cookie": ["a=1", "b=2; HttpOnly;", "c=3; max-age=5;"]
     });
 
-    // 二级域名访问一级域名cookie
-    // a.test.com  b.test.com
-    // if (url === "test.com") {
+    // 2. 设置二级域名访问一级域名cookie
+    // if (url === "http://test.com/") {
     //   response.writeHead(200, {
     //     "Content-Type": "text/html",
     //     "Set-Cookie": ["d=4; domain=test.com"]
